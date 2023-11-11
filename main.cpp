5e2ef38e2c307f772206e19a979d16d7cddfeffb
jbrhm <134429827+jbrhm@users.noreply.github.com>
2023-11-10 19:53:28 -0500

    Wowrking Video Capture

--- main.cpp
+++ main.cpp
@@ -2,23 +2,17 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 using namespace cv;
-int main(int argc, char** argv )
+int main()
 {
-    printf("BURAHWODHOI");
-    if ( argc != 2 )
-    {
-        printf("usage: DisplayImage.out <Image_Path>\n");
-        return -1;
-    }
+    VideoCapture cap(0);
     Mat image;
-    image = imread( argv[1], IMREAD_COLOR );
-    if ( !image.data )
-    {
-        printf("No image data \n");
-        return -1;
-    }
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
-    waitKey(0);
+    while(true){
+        if(cap.read(image)){
+            namedWindow("Display Image", WINDOW_AUTOSIZE );
+            imshow("Display Image", image);
+            waitKey(1);
+        }
+    }  
+    
     return 0;
 }
